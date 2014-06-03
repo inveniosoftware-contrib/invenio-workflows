5c54d4435ec9b1cd4c16cc0555187071fd872adc
Tibor Simko <tibor.simko@cern.ch>
2014-06-03 13:38:27 +0200

    global: file renaming
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
+{#
 ## This file is part of Invenio.
-## Copyright (C) 2012 CERN.
+## Copyright (C) 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -14,3 +15,5 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+#}
+{% include 'websearch_index.html' %}
