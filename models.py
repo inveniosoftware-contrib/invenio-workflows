nikpap/invenio

workflows: current task in Holding Pen details
* BETTER Updates the layout of the details pages in Holding Pen to
  display at which step the object is in the workflow.

* Adds a new method to the BibWorkflowObject model to get the current
  task information.

* Performs unicode conversion in several templates of the details pages
  to address several warnings about non-unicode strings.

Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

invenio/modules/workflows/models.py
@@ -574,7 +574,7 @@ class BibWorkflowObject(db.Model):
     def get_action_message(self):
         """Retrieve the currently assigned widget, if any."""
         try:
-            return self.get_extra_data()["_message"]
+            return unicode(self.get_extra_data()["_message"])
         except KeyError:
             # No widget
             return ""
@@ -688,6 +688,20 @@ class BibWorkflowObject(db.Model):
         except KeyError:
             # Assume old version "task_counter"
             return extra_data["task_counter"]
+
+    def get_current_task_info(self):
+        """Return a dictionary of current task function info for this object."""
+        from .utils import get_workflow_definition, get_func_info
+
+        task_pointer = self.get_current_task()
+        name = self.get_workflow_name()
+        if not name:
+            return ""
+        current_task = get_workflow_definition(name)
+        for step in task_pointer:
+            current_task = current_task[step]
+            if callable(current_task):
+                return get_func_info(current_task)
 
     def save_to_file(self, directory=None,
                      prefix="workflow_object_data_", suffix=".obj"):

inveniosoftware-contrib/invenio-workflows

Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>

models.py
@@ -26,6 +26,7 @@ import tempfile
 from datetime import datetime
 
 from invenio.base.globals import cfg
+from invenio.base.helpers import unicodifier
 from invenio.base.utils import classproperty
 from invenio.ext.logging import deprecated
 from invenio.ext.sqlalchemy import db
@@ -574,7 +575,7 @@ class BibWorkflowObject(db.Model):
     def get_action_message(self):
         """Retrieve the currently assigned widget, if any."""
         try:
-            return self.get_extra_data()["_message"]
+            return unicodifier(self.get_extra_data()["_message"])
         except KeyError:
             # No widget
             return ""
@@ -688,6 +689,20 @@ class BibWorkflowObject(db.Model):
         except KeyError:
             # Assume old version "task_counter"
             return extra_data["task_counter"]
+
+    def get_current_task_info(self):
+        """Return a dictionary of current task function info for this object."""
+        from .utils import get_workflow_definition, get_func_info
+
+        task_pointer = self.get_current_task()
+        name = self.get_workflow_name()
+        if not name:
+            return ""
+        current_task = get_workflow_definition(name)
+        for step in task_pointer:
+            current_task = current_task[step]
+            if callable(current_task):
+                return get_func_info(current_task)
 
     def save_to_file(self, directory=None,
                      prefix="workflow_object_data_", suffix=".obj"):